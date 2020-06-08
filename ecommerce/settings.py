17d8027eae199b5e600f2d2acbdba8814085da3f
Nicolò <237978@studenti.unimore.it>
2020-06-08 20:21:15 +0200

    Aggiunta carrello

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.StoreConfig'
+    'store.apps.StoreConfig',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/images')
